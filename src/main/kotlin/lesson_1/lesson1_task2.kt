33193a4f934e8c76d33cd99cc0b127848640332f
Alex Prince <zhuzepa@yandex.ru>
2025-12-03 22:41:13 +0300

    KS-1-2 добавил numberEmployess

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,19 @@
+package lesson_1
+
+fun main() {
+    val orders: Int = 75
+    val thanksText: String = "Спасибо что выбираете нас"
+
+    println(orders)
+    println(thanksText)
+
+    var numberEmployess: Int = 2000
+
+//    println(numberEmployess)
+
+    numberEmployess = 1999
+
+    println(numberEmployess)
+
+
+}
