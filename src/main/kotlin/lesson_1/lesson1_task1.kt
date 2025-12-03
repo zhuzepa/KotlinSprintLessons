7d83430769681d35bf6b66c4f9487b4039b7931b
Alex Prince <zhuzepa@yandex.ru>
2025-12-03 12:41:52 +0300

    KS-1-1 добавил 2 неизменяемых переменных

--- src/main/kotlin/lesson_1/lesson1_task1.kt
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -1,8 +1,8 @@
 package lesson_1
 
 fun main() {
-    var orders = 75
-    var thanksText = "Спасибо что выбираете нас"
+    val orders: Int = 75
+    val thanksText: String = "Спасибо что выбираете нас"
 
     println(orders)
     println(thanksText)
